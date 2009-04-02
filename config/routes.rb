cb84adb0d0f80201344115ad6757fa5e9b7c75d5
Integrum User <integrum@SayWhatAgain.local>
2009-04-01 19:34:18 -0700

    Adding default route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ ActionController::Routing::Routes.draw do |map|
   # The priority is based upon order of creation: first created -> highest priority.
 
   map.resources :interns
-  
+  map.root :controller => 'interns'
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 end
